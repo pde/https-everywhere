15f78bd573ba573dde9e630f053a3fa0f47446ee
Yan <yan@mit.edu>
2013-07-13 09:54:55 +0200

    Testing toolbar rule toggling

--- src/chrome/content/toolbar_button.js
+++ src/chrome/content/toolbar_button.js
@@ -222,6 +222,23 @@ function show_applicable_list(menupopup) {
 function toggle_rule(rule_id) {
   // toggle the rule state
   HTTPSEverywhere.https_rules.rulesetsByID[rule_id].toggle();
+  var prefs = HTTPSEverywhere.get_prefs();
+  var report_global = prefs.getBoolPref("report_disabled_rules_global");
+  var report_specific = prefs.getBoolPref("report_disabled_rules_specific");
+
+  if (report_global) {
+    alert('will automatically report this rule');
+//  probably need something like
+//  var aWin = CC['@mozilla.org/appshell/window-mediator;1']
+//  .getService(CI.nsIWindowMediator) 
+//  .getMostRecentWindow('navigator:browser');
+//  aWin.openDialog("chrome://https-everywhere/content/fetch-source.xul",
+//                   rs.xmlName, "chrome,centerscreen", 
+//                   {xmlName: rs.xmlName, GITCommitID: GITID} );
+  } else if (report_specific) {
+    alert('will ask user whether to report this rule');
+  }
+
   var domWin = content.document.defaultView.top;
   /*if (domWin instanceof CI.nsIDOMWindow) {
     var alist = HTTPSEverywhere.getExpando(domWin,"applicable_rules", null);
